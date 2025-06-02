5a202d62e6ef5b8287a3ce04cab37176598845e8
Giovanni Borgogno <giovaborgogno@gmail.com>
2025-06-02 11:49:43 -0300

    Update bugster API URL to point to the production environment

--- bugster/libs/settings.py
+++ bugster/libs/settings.py
@@ -12,7 +12,7 @@ class LibsSettings(BaseSettings):
     def bugster_api_url(self) -> str:
         """Return the API URL based on the environment."""
         # TODO: See if we can use the environment variable to set the API URL
-        return "https://dev.bugster.app"
+        return "https://api.bugster.app"
 
     environment: str = Field(default="localhost")
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
